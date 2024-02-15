3282321a393e683f83980e8b8fc2a1f8b0a63bd3
RolandBEC <bec.roland89@gmail.com>
2024-02-15 20:05:27 +0100

    Add main properties

--- MainWindowController.cs
+++ MainWindowController.cs
@@ -8,6 +8,8 @@ namespace TextToSpeechServiceConsumer
 {
     internal class MainWindowController
     {
+        const string RequestURL = @"https://voicerss-text-to-speech.p.rapidapi.com/";
+
         public MainWindowVM ViewModel { get; } = new MainWindowVM();
 
 
--- MainWindowVM.cs
+++ MainWindowVM.cs
@@ -4,5 +4,40 @@ namespace TextToSpeechServiceConsumer
 {
     public class MainWindowVM : BaseViewModel
     {
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set => SetProperty(ref _text, value);
+        }
+
+        private string _language;
+        public string Language
+        {
+            get => _language;
+            set => SetProperty(ref _language, value);
+        }
+
+        private int _speechRate;
+        public int SpeechRate
+        {
+            get => _speechRate;
+            set => SetProperty(ref _speechRate, value);
+        }
+
+        private string _speechAudioCodec;
+        public string SpeechAudioCodec
+        {
+            get => _speechAudioCodec;
+            set => SetProperty(ref _speechAudioCodec, value);
+        }
+
+        private string _speechAudioFormat;
+        public string SpeechAudioFormat
+        {
+            get => _speechAudioFormat;
+            set => SetProperty(ref _speechAudioFormat, value);
+        }
+
     }
 }
